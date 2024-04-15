44fb4d1a8c8b5b981d9d8e79924d89c7f1c97891
Thomas Vincent <thomasvincent@gmail.com>
2024-04-15 02:20:02 -0700

    Update clean-build-history.groovy
    auto generated javadoc to puts some clarity to the madness

--- clean-build-history.groovy
+++ clean-build-history.groovy
@@ -5,7 +5,11 @@ import org.kohsuke.stapler.DataBoundConstructor
 import java.util.logging.Logger
 
 /**
- * Class to clean up Jenkins jobs and optionally reset their build numbers.
+ * Cleans Jenkins jobs either by removing old builds or by resetting their build numbers.
+ * This utility class allows for targeted clean-up operations within a Jenkins instance,
+ * affecting only specified jobs or folders.
+ * 
+ * @author Author
  */
 class JobCleaner {
     private static final Logger LOGGER = Logger.getLogger(JobCleaner.class.name)
@@ -18,6 +22,15 @@ class JobCleaner {
     private final int cleanedJobsLimit
     private final int buildTotal
 
+    /**
+     * Data-bound constructor to create a JobCleaner object.
+     * 
+     * @param jenkins the Jenkins instance this cleaner will operate on
+     * @param jobName the name of the job to clean
+     * @param resetBuildNumber if true, resets the build number to 1
+     * @param cleanedJobsLimit the maximum number of jobs to clean within a folder
+     * @param buildTotal the maximum number of builds to delete from a job
+     */
     @DataBoundConstructor
     JobCleaner(Jenkins jenkins, String jobName, boolean resetBuildNumber, int cleanedJobsLimit = DEFAULT_CLEANED_JOBS_LIMIT, int buildTotal = DEFAULT_BUILD_TOTAL) {
         this.jenkins = jenkins ?: throw new IllegalArgumentException("Jenkins instance cannot be null")
@@ -27,6 +40,10 @@ class JobCleaner {
         this.buildTotal = buildTotal
     }
 
+    /**
+     * Initiates the cleaning process for the specified job or folder.
+     * It checks if the item is a folder or a job and applies the appropriate cleaning method.
+     */
     void clean() {
         def item = jenkins.getItemByFullName(jobName)
         if (item instanceof AbstractFolder) {
@@ -38,12 +55,22 @@ class JobCleaner {
         }
     }
 
+    /**
+     * Cleans jobs within a folder, limiting the number of cleaned jobs to the specified limit.
+     * 
+     * @param folder the folder to clean jobs within
+     */
     private void cleanFolder(AbstractFolder folder) {
         folder.getItems(AbstractItem.class).take(cleanedJobsLimit).each { subJob ->
             new JobCleaner(jenkins, subJob.name, resetBuildNumber).clean()
         }
     }
 
+    /**
+     * Deletes builds from a job up to the specified limit and resets the build number if specified.
+     * 
+     * @param job the job from which builds will be deleted
+     */
     private void cleanJob(Job job) {
         boolean buildsDeleted = false
         job.getBuilds().take(buildTotal).each { build ->
